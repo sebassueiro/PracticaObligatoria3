e6ddcb6a49054da93ebd5ce8dac3d52218d95713
sebatian sueiro <s.sueiro@hotmail.com>
2023-09-19 11:08:10 -0300

    practica3

---
+++ src/components/Login/Login.js
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from 'react'
+
+const Login = () => {
+    const userRef = useRef(null);
+    const [user , setUser ] = useState ("");
+
+    const userChangeHandler = (event) =>{
+      setUser(event.target.value);
+      if(event.target.value.includes("o") || event.target.value.includes("O")){
+        alert("Por favor, ¡Nombres de usuario sin la letra o! ");
+            return;
+      }
+    };
+
+    const signInHandler = () => {
+        if (user.length <=  0 || user.includes("o") || user.includes("O")){
+            alert("Usuario inválido para registrarse ");
+            return;
+        }
+        else {
+          alert("¡Usuario registrado correctamente! ")
+        }
+    };
+
+  return (
+    <div>
+      <p>Ingrese su usuario:</p>
+      <input 
+      type='text'
+      onChange={userChangeHandler}
+      value={user}
+      ref={userRef}
+      placeholder='ingrese su usuario'
+      />
+      <button onClick={signInHandler} type='button'>
+        Registrarse
+        </button>
+      <p>{user}</p>
+    </div>
+  )
+}
+
+export default Login
